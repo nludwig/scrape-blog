589681a6cbd59e15f64b9887daea22e5e847a447
nludwig <nick.b.ludwig@gmail.com>
2020-11-22 17:43:09 -0800

    Fix some bugs in `setup.py`

--- setup.py
+++ setup.py
@@ -2,15 +2,26 @@
 
 import setuptools
 
+readme = 'README.md'
+with open(readme) as f:
+    long_description = f.read()
+
 setuptools.setup(
-    'name': 'scrape-blog',
-    'version': 0.0.1,
-    'description': 'Tool to scrape SSC and maybe other simple blogs.',
-    'author': 'Nick Ludwig',
-    'author_email': 'nick.b.ludwig@gmail.com',
-    'install_requires': [
+    name='scrape-blog',
+    version='0.0.1',
+    author='Nick Ludwig',
+    author_email='nick.b.ludwig@gmail.com',
+    description='Tool to scrape SSC and maybe other simple blogs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nludwig/scrape-blog',
+    packages=setuptools.find_packages(),
+    install_requires=[
         'beautifulsoup4',
         'python-docx',
         'requests',
     ],
+    entry_points={
+        'console_scripts': ['scrape-ssc = scrape_blog.scrape_ssc:main'],
+    },
 )
